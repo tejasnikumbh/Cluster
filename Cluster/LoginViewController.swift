d8d778ea1917153591ad2e4df1ea5d046193d8a8
tejasnikumbh <tejasnikumbh@gmail.com>
2015-12-18 15:19:43 +0530

    Empty Stubs for singup and login flow

---
+++ Cluster/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  Cluster
+//
+//  Created by Tejas Nikumbh on 12/18/15.
+//  Copyright © 2015 Personal. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var bgView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ Cluster/SignUpViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpViewController.swift
+//  Cluster
+//
+//  Created by Tejas Nikumbh on 12/18/15.
+//  Copyright © 2015 Personal. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var bgView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
