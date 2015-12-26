5b3fe5ecb64c0e97035da18c34e698e7bf74668c
tejasnikumbh <tejasnikumbh@gmail.com>
2015-12-26 09:21:34 +0530

    Primary Signup product changes complete

--- Cluster/SignUpFormViewController.swift
+++ Cluster/SignUpFormViewController.swift
@@ -19,18 +19,18 @@ class SignUpFormViewController: CSFormBaseViewController {
     
     @IBOutlet weak var fullNameTextField: UITextField!
     @IBOutlet weak var phoneNumberTextField: UITextField!
-    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var designationTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var confirmPasswordTextField: UITextField!
     
     @IBAction func submitBtnTapped(sender: UIButton) {
         let fullName = self.fullNameTextField.text
+        let designation = self.designationTextField.text
         let phoneNumber = self.phoneNumberTextField.text
-        let email = self.emailTextField.text
         let password = self.passwordTextField.text
         // TODO: Validate all these (including if password matches confirm password)
-        self.signUpUser(email, password: password,
-            fullName: fullName, phoneNumber: phoneNumber)
+        self.signUpUser(phoneNumber, fullName: fullName, designation: designation,
+            phoneNumber: phoneNumber, password: password)
     }
 
     /* ======================================== Super Methods Overridden ================================== */
@@ -62,14 +62,14 @@ class SignUpFormViewController: CSFormBaseViewController {
 // Extension for parse methods
 extension SignUpFormViewController {
     
-    func signUpUser(email: String?, password: String?,
-        fullName:String?, phoneNumber: String?) {
+    func signUpUser(username: String?, fullName: String?,
+        designation:String?, phoneNumber: String?, password: String?) {
         let newUser = PFUser()
-        newUser.username = email
+        newUser.username = username
         newUser.password = password
-        newUser.setObject(email!, forKey: "primary_email")
-        newUser.setObject(phoneNumber!, forKey: "primary_phone")
         newUser.setObject(fullName!, forKey: "full_name")
+        newUser.setObject(designation!, forKey: "designation")
+        newUser.setObject(phoneNumber!, forKey: "primary_phone")
         let spinner = CSUtils.startSpinner(self.view)
         newUser.signUpInBackgroundWithBlock(
         {
